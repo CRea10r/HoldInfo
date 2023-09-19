a201a650e10e734437a92c8973c194a3e5355c3d
CRea10r <125733574+CRea10r@users.noreply.github.com>
2023-09-19 17:25:28 +0530

    Add files via upload

---
+++ public/script.js
@@ -0,0 +1,46 @@
+const loader = document.getElementById('loader');
+const table = document.getElementById('table');
+
+async function fetchData() {
+    try {
+        const response = await fetch('/api/tickers', { method: 'GET' });
+        const data = await response.json();
+        populateTable(data.tickers);
+    } catch (error) {
+        console.error(error);
+    } finally {
+        loader.classList.add('hidden');
+    }
+}
+
+function populateTable(tickers) {
+    const tableBody = document.querySelector('#table tbody');
+    table.classList.remove('hidden');
+    tableBody.innerHTML = '';
+
+    tickers.forEach(ticker => {
+        const row = document.createElement('tr');
+        row.classList.add('ticker-row');
+        row.innerHTML = `
+            <td>${ticker.name}</td>
+            <td>${ticker.last}</td>
+            <td>${ticker.buy}</td>
+            <td>${ticker.sell}</td>
+            <td>${ticker.volume}</td>
+            <td>${ticker.base_unit}</td>
+        `;
+        tableBody.appendChild(row);
+        tableBody.appendChild(document.createElement('br'));
+    });
+}
+
+window.onload = function () {
+    loader.classList.remove('hidden');
+    table.classList.add('hidden');
+    fetchData();
+
+    setInterval(() => {
+        fetchData();
+    }, 2000);
+};
+    
